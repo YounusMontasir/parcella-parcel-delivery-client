291ee33051c5aff6b92e590e0f2c30025edd187f
YounusMontasir <ymontasir73@gmail.com>
2025-01-14 11:26:47 +0600

    initial commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='h-[600px] bg-blue-500'> 
+            <h2 className='text-4xl ml-52 pt-32'>Deliver the Happiness </h2>
+            <p className='ml-52'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci itaque, maiores aspernatur cupiditate tempore, facere vero ducimus voluptatibus consequuntur culpa magnam quidem nulla eius quod ipsa deserunt inventore obcaecati natus!</p>
+            <button className='btn bg-yellow-700 text-white mx-auto'>Deliver</button>
+
+        </div>
+    );
+};
+
+export default Banner;
